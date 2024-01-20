3763f8f9242de7fd1975061d0704604c55b70926
YAMIL ISMAEL GARCIA NOLASCO <108230634+ismagn@users.noreply.github.com>
2024-01-20 13:36:55 -0600

    Delete front-dr-coffee directory

--- front-dr-coffee/src/hook/useCoffee.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import DrCoffeeContext from "../context/DrCoffeeProvider";
-
-//se crea un hook perzonalizado para poder utilizar lo valores del prop global que se creo
-const useDrCoffee = () => {
-    return useContext(DrCoffeeContext)
-}
-
-export default useDrCoffee
